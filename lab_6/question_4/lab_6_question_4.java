fb7d83dbb16adec2a6a8fdc84706f7204892b282
andy <andy@localhost.localdomain>
2020-02-27 16:32:28 -0800

    lab 6

--- lab_6/question_4/lab_6_question_4.java
+++ lab_6/question_4/lab_6_question_4.java
@@ -25,19 +25,9 @@ class Question_4
             PreparedStatement overtime_pstm = con.prepareStatement(ot_query);
             ResultSet rs = overtime_pstm.executeQuery();
 
+            // Display the overtime hours and overtime pay for each project that and employee is working on
+            // Provided that they have any overtime at all
             System.out.println("EMPLOYEE PROJECT OVERTIME: ");
-
-            String first_emp = "select MIN(empno), MAX(empno) from emp_proj_overtime order by empno";
-            PreparedStatement statemnet = con.prepareStatement(first_emp);
-            //statemnet.setMaxRows(1);
-            ResultSet first = statemnet.executeQuery();
-            first.next();
-            int prev_emp = first.getInt(1);
-            int last_emp = first.getInt(2);
-            //System.out.println(last_emp);
-            //System.out.println(prev_emp);
-            int total_hours = 0;
-            BigDecimal total_pay = new BigDecimal("0");
 
             while (rs.next())
             {
@@ -54,7 +44,7 @@ class Question_4
                 BigDecimal ot_pay = new BigDecimal(rs_hourly.getString("hourly_rate"));
                 BigDecimal ot_hours = new BigDecimal(hourot);
                 BigDecimal twice = new BigDecimal("2");
-                ot_pay = twice.multiply(ot_pay.multiply(ot_hours));
+                ot_pay = ot_pay.multiply(twice.multiply(ot_hours));
 
                 // Print the employee's ID, the project they are working on, their overtime hours for that project
                 // and their hourly overtime pay rate
@@ -66,42 +56,44 @@ class Question_4
 
             rs.close();
 
-
+            // Display the total overtime hours and total overtime pay for each employee
             System.out.println("\n" + "TOTAL OVERTIME HOURS AND TOTAL OVERTIME PAY: ");
-            /*For each empno get the total of their overtime hours and overtime pay*/
+            
+            // Select distince employee numbers from EMP_PROJ_OVERTIME
             String total_query = "select distinct empno from emp_proj_overtime";
             PreparedStatement total_ot = con.prepareStatement(total_query);
             ResultSet rs_total = total_ot.executeQuery();
 
             while(rs_total.next())
             {
-                //System.out.println("hey");
                 int total_empno = rs_total.getInt("empno");
-                System.out.print("EMPNO: " + total_empno + ", ");
 
+                // Get employee's total overtime hours
                 String get_all = "select sum(hourot) from emp_proj_overtime where empno = " + total_empno;
                 PreparedStatement total_emps = con.prepareStatement(get_all);
                 ResultSet rs_total_hours = total_emps.executeQuery();
                 rs_total_hours.next();
-
                 int total_emp_ot_hours = rs_total_hours.getInt(1);
-                System.out.print("TOTAL OVERTIME HOURS: " + total_emp_ot_hours + ", ");
 
+                // Get employees hourly pay rate
                 String get_total_pay = "select hourly_rate from hourly_pay where empno = " + total_empno;
                 PreparedStatement total_pay_statement = con.prepareStatement(get_total_pay);
                 ResultSet rs_total_pay = total_pay_statement.executeQuery();
                 rs_total_pay.next();
 
+                // Calculate employee's total overtime pay
                 BigDecimal pay_rate = new BigDecimal(rs_total_pay.getString("hourly_rate"));
                 BigDecimal total_ot_pay = new BigDecimal(total_emp_ot_hours);
-                //System.out.println(pay_rate);
                 BigDecimal two = new BigDecimal("2");
                 total_ot_pay = total_ot_pay.multiply(two.multiply(pay_rate));
+                
+                // Display results
+                System.out.print("EMPNO: " + total_empno + ", ");
+                System.out.print("TOTAL OVERTIME HOURS: " + total_emp_ot_hours + ", ");
                 System.out.println("TOTAL OVERTIME PAY: $" + total_ot_pay);
             }
 
             rs_total.close();
-
         }
         catch (Exception e)
         {
